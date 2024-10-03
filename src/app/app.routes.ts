738d31bc6802133d8de885ad03c84938b908ec52
chinmay-3040 <108146322+chinmay-3040@users.noreply.github.com>
2024-10-03 22:46:55 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 // import { RouterModule } from '@angular/router';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 import { AboutComponent } from './MyComponents/about/about.component';
-import { LoginComponent } from '../app/login/login.component';
 import { ErrorComponent } from '../app/error/error.component';
 
 
@@ -10,10 +9,8 @@ import { ErrorComponent } from '../app/error/error.component';
 
 
 export const routes: Routes = [
-    { path: 'dashboard', component: TodosComponent },
+    { path: '', component: TodosComponent },
   { path: 'about', component: AboutComponent},
-  { path: 'login', component: LoginComponent},
-  { path: '', component: LoginComponent },
   { path: 'error', component: ErrorComponent},
 
 ];
